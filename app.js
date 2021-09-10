f62379bd88c6d0258dd57c957705b652c0766284
arialghifari <arialghifari99@gmai.com>
2021-09-10 14:30:46 +0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const request = require('postman-request');
+
+// weather temperature
+/* const url = 'http://api.weatherstack.com/current?access_key=09c6ec214c42983b6725984c50e444bf&query=-8.586289651437763,116.08863635574136'; // weather location in kekalik, mataram
+
+request({ url: url, json: true }, (error, response) => {
+    const current = response.body.current;
+    const deg = current.temperature + '°C';
+    const feelsLikeDeg = current.feelslike + '°C';
+    const precipPercentage = current.precip * 100 + '%';
+    const weatherDesc = current.weather_descriptions[0];
+
+    console.log(response.body.current);
+    console.log(weatherDesc + '. It is currently ' + deg + ' out and it feels like ' + feelsLikeDeg + ' out. There is ' + precipPercentage + ' chance of raining.');
+}); */
+
+// Geocoding
+// Address -> Latitude and Longitude
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/mataram.json?access_token=pk.eyJ1IjoiYXJpYWxnaGlmYXJpMjMiLCJhIjoiY2t0ZDFoMXZmMG9tdDJ1bW9jdDdpeWF6ZiJ9.B-2xOFbuZaUaLm8Rf7Wzww&limit=1';
+
+request({ url: geoUrl, json: true }, (error, response) => {
+	const latitude = response.body.features[0].center[1];
+	const longitude = response.body.features[0].center[0];
+
+	console.log(response.body.query[0]);
+	console.log('latitude: ' + latitude);
+	console.log('longitude: ' + longitude);
+});
