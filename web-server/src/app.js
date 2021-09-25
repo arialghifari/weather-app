f9007f643704b47f8debe05211937bf2bbd8f000
arialghifari <arialghifari99@gmail.com>
2021-09-25 11:24:56 +0800

    using hbs

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,32 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public/');
+const publicDirectoryPath = path.join(__dirname, '../public');
 
+app.set('view engine', 'hbs'); //using handlebars .hbs template for dynamic content
+app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(publicDirectoryPath));
+
+app.get('', (req, res) => {
+	res.render('index', {
+		title: 'Weather App',
+		name: 'Ari Alghifari'
+	});
+});
+
+app.get('/about', (req, res) => {
+	res.render('about', {
+		title: 'About Me',
+		name: 'Ari Alghifari'
+	});
+});
+
+app.get('/help', (req, res) => {
+	res.render('help', {
+		title: 'Help',
+		helpText: 'Example help text'
+	});
+});
 
 app.get('/weather', (req, res) => {
 	res.send({
