ce6dbb980c545fdb7be2268e4a6368c57bfa266c
arialghifari <arialghifari99@gmail.com>
2021-12-20 08:17:42 +0800

    V1.0 Weather App

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,5 +1,6 @@
 const weatherForm = document.querySelector('form');
 const searchLocation = document.querySelector('input');
+const message = document.querySelector('.message');
 const messageOne = document.querySelector('#message-1');
 const messageTwo = document.querySelector('#message-2');
 const icon = document.querySelector('.icon');
@@ -7,8 +8,10 @@ const icon = document.querySelector('.icon');
 weatherForm.addEventListener('submit', (e) => {
 	e.preventDefault();
 
+	message.style.display = "flex";
 	messageOne.textContent = 'Loading...';
 	messageTwo.textContent = '';
+	icon.style.display = "none";
 
 	fetch('http://localhost:3000/weather?address=' + searchLocation.value).then((response) => {
 		response.json().then((data) => {
@@ -17,6 +20,7 @@ weatherForm.addEventListener('submit', (e) => {
 			} else {
 				messageOne.textContent = data.forecast;
 				messageTwo.textContent = data.location;
+				icon.style.display = "initial";
 				icon.src = data.icon;
 			}
 		});
